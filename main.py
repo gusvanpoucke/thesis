675544678c89ac5013379b6d22defc38e18f188c
Gus Vanpoucke <159711661+gusvanpoucke@users.noreply.github.com>
2026-04-09 12:32:24 +0200

    wait first experiment results

--- main.py
+++ main.py
@@ -282,32 +282,29 @@ def total_costs(files, folder="experiment_results/standard_vns/"):
     return total_best, total_average, len(files)
 
 if __name__ == "__main__":
-    find_improving_solution("c100.json", 0.0)
-    """
     # 21 files
     list_of_dvrp_files = [
-        #"c100.json",
-        #"c100b.json", 
-        #"c120.json",
-        #"c150.json",
-        #"c199.json",
-        #"c50.json",
-        #"c75.json",
-        #"f134.json",
-        #"f71.json",
-        #"tai100a.json",
-        #"tai100b.json",
-        #"tai100c.json",
-        #"tai100d.json",
-        #"tai150a.json",
-        #"tai150b.json",
-        #"tai150c.json",
-        #"tai150d.json",
+        "c100.json",
+        "c100b.json", 
+        "c120.json",
+        "c150.json",
+        "c199.json",
+        "c50.json",
+        "c75.json",
+        "f134.json",
+        "f71.json",
+        "tai100a.json",
+        "tai100b.json",
+        "tai100c.json",
+        "tai100d.json",
+        "tai150a.json",
+        "tai150b.json",
+        "tai150c.json",
+        "tai150d.json",
         "tai75a.json",
         "tai75b.json",
         "tai75c.json",
         "tai75d.json"
     ]
     for dvrp_file in list_of_dvrp_files:
-        runXTestsOnFile(dvrp_file, results_folder="experiment_results/lazy_vns/")
-    """
+        runXTestsOnFile(dvrp_file, results_folder="experiment_results/wait_first_vns/")
