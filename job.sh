89fb5ba672e179215a560a3f9c28a0110fdcdae4
Gus Vanpoucke <159711661+gusvanpoucke@users.noreply.github.com>
2026-04-16 17:31:20 +0200

    load modules in job file

--- job.sh
+++ job.sh
@@ -5,4 +5,15 @@
 
 cd $PBS_O_WORKDIR
 
+module load numpy
+module load cProfile
+module load pstats
+module load json
+module load time
+module load os
+module load random
+module load re
+module load math
+module load pathlib
+
 python job.py
