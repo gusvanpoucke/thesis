bc07ba41a950a684d132595cd8c85d18680f5c53
Gus Vanpoucke <159711661+gusvanpoucke@users.noreply.github.com>
2026-04-16 17:41:51 +0200

    load only numpy

--- job.sh
+++ job.sh
@@ -6,14 +6,5 @@
 cd $PBS_O_WORKDIR
 
 module load numpy
-module load cProfile
-module load pstats
-module load json
-module load time
-module load os
-module load random
-module load re
-module load math
-module load pathlib
 
 python job.py
