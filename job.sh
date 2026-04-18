4ef20b7262629a0187f75bc1468f37cf5ba8e921
Gus Vanpoucke <159711661+gusvanpoucke@users.noreply.github.com>
2026-04-18 16:04:42 +0200

    retry reduce capacity parameters drive first job with more memory

--- job.sh
+++ job.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-#PBS -N capacity_wait
+#PBS -N capacity_drive
 #PBS -l walltime=32:00:00
 #PBS -l nodes=1:ppn=1
+#PBS -l mem=20gb
 #PBS -t 0-20
 #PBS -o logs/output_${PBS_ARRAYID}.out
 #PBS -e logs/error_${PBS_ARRAYID}.err
@@ -13,4 +14,4 @@ mkdir -p logs
 module load Python/3.11.3-GCCcore-12.3.0
 module load SciPy-bundle/2023.07-gfbf-2023a
 
-python job_capacity_wait.py ${PBS_ARRAYID}
+python job_capacity.py ${PBS_ARRAYID}
