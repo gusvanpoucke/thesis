0bb514de81c9732e807125083f2baadac40a2c7d
Gus Vanpoucke <159711661+gusvanpoucke@users.noreply.github.com>
2026-04-18 12:47:04 +0200

    compare heuristics

--- main.py
+++ main.py
@@ -13,6 +13,15 @@ from VNS import cvrp, event_scheduler
 from repair import repair, split_route
 from dynamic_route import Route
 
+# Your 21 files in order
+list_of_dvrp_files = [
+    "c100.json", "c100b.json", "c120.json", "c150.json", "c199.json",
+    "c50.json", "c75.json", "f134.json", "f71.json", "tai100a.json",
+    "tai100b.json", "tai100c.json", "tai100d.json", "tai150a.json",
+    "tai150b.json", "tai150c.json", "tai150d.json", "tai75a.json",
+    "tai75b.json", "tai75c.json", "tai75d.json"
+]
+
 def check_parameters(file_name, alpha, epsilon, results_folder="experiment_results/fullness_parameters/",
     number_of_tests=30, waiting_strategy="wait_first", termination_time=5
 ):
@@ -270,13 +279,33 @@ def total_costs(files, folder="experiment_results/standard_vns/"):
     
     return total_best, total_average, len(files)
 
+def compare_heuristics(heuristic_folder, comparison_folder):
+    count_improved = 0
+    total_relative_improvement = 0.0
+    for dvrp_file in list_of_dvrp_files:
+        heuristic_file = heuristic_folder + dvrp_file
+        comparison_file = comparison_folder + dvrp_file
+
+        with open(heuristic_file, 'r') as file:
+            heuristic_average = json.load(file)['average_cost']
+        with open(comparison_file, 'r') as file:
+            comparison_average = json.load(file)['average_cost']
+
+        if heuristic_average < comparison_average:
+            count_improved += 1
+        total_relative_improvement += heuristic_average / comparison_average
+    average_relative_improvement = total_relative_improvement / len(list_of_dvrp_files)
+
+    # write to file
+    data = {
+        "heuristic": heuristic_folder,
+        "comparison": comparison_folder,
+        "count_improved": count_improved,
+        "average_relative_improvement": average_relative_improvement
+    }
+    json_filename = f"{heuristic_folder}compare_heuristics.json"
+    with open(json_filename, "w") as json_file:
+        json.dump(data, json_file, indent=4)
+
 if __name__ == "__main__":
-    alphas = [2.0, 4.0, 6.0, 8.0, 10.0]
-    epsilons = [0.0, 1.0, 2.0, 3.0, 4.0]
-    for alpha in alphas:
-        for epsilon in epsilons:
-            if alpha <= 6.0 or alpha == 8.0 and epsilon <= 1.0:
-                continue
-            print(alpha, epsilon)
-            check_parameters("c150.json", alpha, epsilon)
-            print()
+    compare_heuristics("experiment_results/wait_first_vns/", "experiment_results/standard_vns/")
