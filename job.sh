6f03a59d408053cab8c2a799324e303083f115c7
Gus Vanpoucke <159711661+gusvanpoucke@users.noreply.github.com>
2026-05-27 15:42:52 +0200

    closest first wait margin fullness job

--- job.sh
+++ job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -N clock_full_margin
+#PBS -N close_full_margin
 #PBS -l walltime=02:00:00
 #PBS -l nodes=1:ppn=1
 #PBS -t 0-20
@@ -13,4 +13,4 @@ mkdir -p logs
 module load Python/3.11.3-GCCcore-12.3.0
 module load SciPy-bundle/2023.07-gfbf-2023a
 
-python job_clockwise_wait_margin_fullness.py ${PBS_ARRAYID}
+python job_closest_first_wait_margin_fullness.py ${PBS_ARRAYID}
